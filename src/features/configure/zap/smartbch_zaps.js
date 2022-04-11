73d604a8bd1f29e46811a0e46c429392719ab89f
Trankuilized <mvinathan@gmail.com>
2022-04-11 13:25:59 -0400

    1st bch go - woot woot

---
+++ src/features/configure/zap/smartbch_zaps.js
@@ -0,0 +1,9 @@
+export const bscZaps = [
+  {
+    // Mistswap
+    zapAddress: '0x3229bC8cE832360692cf5f0F04726533C8EA14E3',
+    ammRouter: '0x5d0bF8d8c8b054080E2131D8b260a5c6959411B8',
+    ammFactory: '0x5d0bF8d8c8b054080E2131D8b260a5c6959411B8',
+    ammPairInitHash: '0x00fb7f630766e6a796048ea87d01acd3068e8ff67d078148a3fa3f4a84f69bd5',
+  },
+];
