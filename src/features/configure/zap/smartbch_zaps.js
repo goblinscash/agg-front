2609ebec095bdcec50178257825e3f40736dc758
Trankuilized <mvinathan@gmail.com>
2022-04-11 13:33:28 -0400

    1st bch go - woot woot

--- src/features/configure/zap/smartbch_zaps.js
+++ src/features/configure/zap/smartbch_zaps.js
@@ -4,6 +4,6 @@ export const smartbchZaps = [
     zapAddress: '0x3229bC8cE832360692cf5f0F04726533C8EA14E3',
     ammRouter: '0x5d0bF8d8c8b054080E2131D8b260a5c6959411B8',
     ammFactory: '0x5d0bF8d8c8b054080E2131D8b260a5c6959411B8',
-    ammPairInitHash: '0x00fb7f630766e6a796048ea87d01acd3068e8ff67d078148a3fa3f4a84f69bd5',
+    ammPairInitHash: '0x3f25a8f0fabcd4621dd46790e10272d91ab392dc12e87e993ca4ce7e7e525ea3',
   },
 ];
